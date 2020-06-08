b91c66f18ddca57f8893bf34e0ba4b3ed5c1e090
Lewin Chan <lewin.chan@adaptris.com>
2020-06-08 14:04:30 +0100

    SNYK: commons codec bump
    Turns out that commons-codec 1.14 now throws an IAE if the
    string is invalid base64, and our default test string isn't base64
    valid...

--- interlok-azure-cosmosdb/src/test/java/com/adaptris/interlok/azure/cosmosdb/CosmosAuthorizationHeaderTest.java
+++ interlok-azure-cosmosdb/src/test/java/com/adaptris/interlok/azure/cosmosdb/CosmosAuthorizationHeaderTest.java
@@ -3,6 +3,8 @@ package com.adaptris.interlok.azure.cosmosdb;
 import static com.adaptris.core.ServiceCase.execute;
 import static org.junit.Assert.assertTrue;
 import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import org.junit.Test;
 import com.adaptris.core.AdaptrisMessage;
 import com.adaptris.core.AdaptrisMessageFactory;
@@ -10,12 +12,14 @@ import com.adaptris.core.ServiceException;
 
 public class CosmosAuthorizationHeaderTest {
 
+  public static final String DUMMY_MASTER_KEY = Base64.getEncoder()
+      .encodeToString("my-master-key".getBytes(StandardCharsets.UTF_8));
 
   @Test
   public void testService() throws Exception {
     CosmosAuthorizationHeader service =
         new CosmosAuthorizationHeader().withResourceId("dbs/MyDatabase/colls/MyCollection").withResourceType("colls")
-            .withMasterKey("my-master-key (could be encoded)")
+            .withMasterKey(DUMMY_MASTER_KEY)
             .withHttpVerb("PUT").withTargetKey("AuthToken");
     AdaptrisMessage msg = AdaptrisMessageFactory.getDefaultInstance().newMessage();
     execute(service, msg);
