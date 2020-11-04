3cb4716caad296e76057a01ab03bbd6b6693dac6
Ashley Morgan Anderson <amanderson@albinoloverats.net>
2020-11-04 12:27:01 +0000

    INTERLOK-3196 Outlook/Mail component
    Bring over the Office365 Outlook consumer/producer from interlok-mail.

---
+++ interlok-azure-mail/src/main/java/com/adaptris/interlok/azure/mail/O365MailConsumer.java
@@ -0,0 +1,289 @@
+/*
+ * Copyright 2015 Adaptris Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package com.adaptris.interlok.azure.mail;
+
+import com.adaptris.annotation.AdapterComponent;
+import com.adaptris.annotation.AdvancedConfig;
+import com.adaptris.annotation.ComponentProfile;
+import com.adaptris.annotation.DisplayOrder;
+import com.adaptris.annotation.InputFieldDefault;
+import com.adaptris.annotation.InputFieldHint;
+import com.adaptris.core.AdaptrisMessage;
+import com.adaptris.core.AdaptrisPollingConsumer;
+import com.adaptris.core.CoreException;
+import com.adaptris.core.MultiPayloadAdaptrisMessage;
+import com.adaptris.core.util.DestinationHelper;
+import com.microsoft.aad.msal4j.ClientCredentialFactory;
+import com.microsoft.aad.msal4j.ClientCredentialParameters;
+import com.microsoft.aad.msal4j.ConfidentialClientApplication;
+import com.microsoft.aad.msal4j.IAuthenticationResult;
+import com.microsoft.graph.models.extensions.Attachment;
+import com.microsoft.graph.models.extensions.FileAttachment;
+import com.microsoft.graph.models.extensions.IGraphServiceClient;
+import com.microsoft.graph.models.extensions.InternetMessageHeader;
+import com.microsoft.graph.models.extensions.Message;
+import com.microsoft.graph.requests.extensions.GraphServiceClient;
+import com.microsoft.graph.requests.extensions.IAttachmentCollectionPage;
+import com.microsoft.graph.requests.extensions.IAttachmentRequest;
+import com.microsoft.graph.requests.extensions.IMessageCollectionPage;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.lang3.BooleanUtils;
+
+import javax.mail.BodyPart;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.util.ByteArrayDataSource;
+import javax.validation.constraints.NotBlank;
+import java.io.ByteArrayInputStream;
+import java.util.Base64;
+import java.util.Collections;
+
+/**
+ * Implementation of an email consumer that is geared towards Microsoft
+ * Office 365, using their Graph API and OAuth2.
+ *
+ * @config office-365-mail-consumer
+ */
+@XStreamAlias("office-365-mail-consumer")
+@AdapterComponent
+@ComponentProfile(summary = "Pickup email from a Microsoft Office 365 account using the Microsoft Graph API", tag = "consumer,email,o365,microsoft,office,outlook,365")
+@DisplayOrder(order = { "applicationId", "tenantId", "clientSecret", "username", "delete" })
+public class O365MailConsumer extends AdaptrisPollingConsumer
+{
+  private static final String SCOPE = "https://graph.microsoft.com/.default";
+
+  @Getter
+  @Setter
+  @NotBlank
+  private String applicationId;
+
+  @Getter
+  @Setter
+  @NotBlank
+  private String tenantId;
+
+  @Getter
+  @Setter
+  @NotBlank
+  private String clientSecret;
+
+  @Getter
+  @Setter
+  @NotBlank
+  private String username;
+
+  @Getter
+  @Setter
+  @AdvancedConfig
+  @InputFieldHint(friendly = "Delete messages instead of marking them as read?")
+  @InputFieldDefault("false")
+  private Boolean delete;
+
+  private transient ConfidentialClientApplication confidentialClientApplication;
+
+  @Override
+  protected void prepareConsumer() throws CoreException
+  {
+    try
+    {
+      confidentialClientApplication = ConfidentialClientApplication.builder(applicationId,
+          ClientCredentialFactory.createFromSecret(clientSecret))
+          .authority(tenant())
+          .build();
+    }
+    catch (Exception e)
+    {
+      log.error("Could not identify Azure application or tenant", e);
+      throw new CoreException(e);
+    }
+  }
+
+  @Override
+  protected int processMessages()
+  {
+    log.debug("Polling for mail in Office365 as user " + username);
+
+    int count = 0;
+    try
+    {
+      IAuthenticationResult iAuthResult = confidentialClientApplication.acquireToken(ClientCredentialParameters.builder(Collections.singleton(SCOPE)).build()).join();
+      log.trace("Access token: " + iAuthResult.accessToken());
+      IGraphServiceClient graphClient = GraphServiceClient.builder().authenticationProvider(request -> request.addHeader("Authorization", "Bearer " + iAuthResult.accessToken())).buildClient();
+
+      // TODO Allow the end user to choose the folder and filter themselves
+      IMessageCollectionPage messages = graphClient.users(username).mailFolders("inbox").messages().buildRequest().filter("isRead eq false").get();
+
+      // TODO handle multiple pages...
+      log.debug("Found {} messages", messages.getCurrentPage().size());
+      for (Message outlookMessage : messages.getCurrentPage())
+      {
+        String id = outlookMessage.id;
+        AdaptrisMessage adaptrisMessage = getMessageFactory().newMessage(outlookMessage.body.content);
+
+        if (adaptrisMessage instanceof MultiPayloadAdaptrisMessage)
+        {
+          ((MultiPayloadAdaptrisMessage)adaptrisMessage).setCurrentPayloadId(id);
+        }
+        adaptrisMessage.addMetadata("EmailID", id);
+        adaptrisMessage.addMetadata("Subject", outlookMessage.subject);
+        adaptrisMessage.addMetadata("To", outlookMessage.toRecipients.stream().map(r -> r.emailAddress.address).reduce((a, b) -> a + "," + b).get() );
+        adaptrisMessage.addMetadata("From", outlookMessage.from.emailAddress.address);
+        adaptrisMessage.addMetadata("CC", String.join(",", outlookMessage.ccRecipients.stream().map(r -> r.emailAddress.address).toArray(String[]::new)));
+
+        log.debug("Processing email from {}: {}", outlookMessage.from.emailAddress.address, outlookMessage.subject);
+
+        if (adaptrisMessage instanceof MultiPayloadAdaptrisMessage && outlookMessage.hasAttachments)
+        {
+          MultiPayloadAdaptrisMessage multiPayloadAdaptrisMessage = (MultiPayloadAdaptrisMessage)adaptrisMessage;
+          IAttachmentCollectionPage attachments = graphClient.users(username).messages(id).attachments().buildRequest().get();
+          log.debug("Message has {} attachments", attachments.getCurrentPage().size());
+          for (Attachment reference : attachments.getCurrentPage())
+          {
+            log.debug("Attachment {} is of type {} with size {}", reference.name, reference.oDataType, reference.size);
+            IAttachmentRequest request = graphClient.users(username).messages(id).attachments(reference.id).buildRequest();//new QueryOption("$value", ""));
+            log.debug("URL: {}", request.getRequestUrl());
+            Attachment attachment = request.get();
+            if (attachment instanceof FileAttachment)
+            {
+              FileAttachment file = (FileAttachment)attachment;
+              log.debug("File {} :: {} :: {}", file.name, file.contentType, file.size);
+              if (file.contentType.startsWith("multipart"))
+              {
+                MimeMultipart mimeMultipart = new MimeMultipart(new ByteArrayDataSource(file.contentBytes, file.contentType));
+                parseMimeMultiPart(multiPayloadAdaptrisMessage, mimeMultipart);
+              }
+              else
+              {
+                addAttachmentToAdaptrisMessage(multiPayloadAdaptrisMessage, file.name, file.contentBytes);
+              }
+            }
+          }
+          multiPayloadAdaptrisMessage.switchPayload(id);
+        }
+
+        /*
+         * The internetMessageHeaders need to be requested explicitly with a SELECT.
+         */
+        outlookMessage = graphClient.users(username).messages(id).buildRequest().select("internetMessageHeaders").get();
+        if (outlookMessage.internetMessageHeaders != null)
+        {
+          for (InternetMessageHeader header : outlookMessage.internetMessageHeaders)
+          {
+            adaptrisMessage.addMetadata(header.name, header.value);
+          }
+        }
+
+        retrieveAdaptrisMessageListener().onAdaptrisMessage(adaptrisMessage);
+
+        if (delete())
+        {
+          graphClient.users(username).messages(id).buildRequest().delete();
+        }
+        else
+        {
+          /*
+           * With PATCH, only send what we've changed, in this instance
+           * we're marking the mail as read.
+           */
+          outlookMessage = new Message();
+          outlookMessage.isRead = true;
+          graphClient.users(username).messages(id).buildRequest().patch(outlookMessage);
+        }
+
+        count++;
+      }
+
+    }
+    catch (Throwable e)
+    {
+      log.error("Exception processing Outlook message", e);
+    }
+
+    return count;
+  }
+
+  @Override
+  protected String newThreadName()
+  {
+    return DestinationHelper.threadName(retrieveAdaptrisMessageListener(), null);
+  }
+
+  private String tenant()
+  {
+    return String.format("https://login.microsoftonline.com/%s", tenantId);
+  }
+
+  private boolean delete()
+  {
+    return BooleanUtils.toBooleanDefaultIfNull(delete, false);
+  }
+
+  private void addAttachmentToAdaptrisMessage(MultiPayloadAdaptrisMessage message, String name, byte[] attachment)
+  {
+    try
+    {
+      /*
+       * The Graph API documentation says that contentBytes
+       * is Base64 encoded, but that doesn't always appear to
+       * be the case
+       */
+      attachment = Base64.getDecoder().decode(attachment);
+    }
+    catch (Exception e)
+    {
+      // do nothing; content wasn't base64 encoded
+    }
+    message.addPayload(name, attachment);
+  }
+
+  private void parseMimeMultiPart(MultiPayloadAdaptrisMessage message, MimeMultipart mimeMultipart) throws Exception
+  {
+    for (int i = 0; i < mimeMultipart.getCount(); i++)
+    {
+      BodyPart bodyPart = mimeMultipart.getBodyPart(i);
+      String name = bodyPart.getFileName();
+      Object content = bodyPart.getContent();
+      if (content instanceof MimeMultipart)
+      {
+        parseMimeMultiPart(message, (MimeMultipart)content);
+      }
+      else if (name == null)
+      {
+        // do nothing; this is the email body
+      }
+      else
+      {
+        byte[] bytes = null;
+        if (content instanceof ByteArrayInputStream)
+        {
+          bytes = ((ByteArrayInputStream)content).readAllBytes();
+        }
+        else if (content instanceof String)
+        {
+          bytes = ((String)content).getBytes();
+        }
+        else
+        {
+          log.warn("Unsupported MIME part {}", bodyPart.getContentType());
+        }
+        addAttachmentToAdaptrisMessage(message, name, bytes);
+      }
+    }
+  }
+}
+
---
+++ interlok-azure-mail/src/main/java/com/adaptris/interlok/azure/mail/O365MailProducer.java
@@ -0,0 +1,277 @@
+package com.adaptris.interlok.azure.mail;
+
+import com.adaptris.annotation.AdapterComponent;
+import com.adaptris.annotation.AdvancedConfig;
+import com.adaptris.annotation.ComponentProfile;
+import com.adaptris.annotation.DisplayOrder;
+import com.adaptris.annotation.InputFieldDefault;
+import com.adaptris.annotation.InputFieldHint;
+import com.adaptris.core.AdaptrisMessage;
+import com.adaptris.core.CoreException;
+import com.adaptris.core.MultiPayloadAdaptrisMessage;
+import com.adaptris.core.ProduceException;
+import com.adaptris.core.ProduceOnlyProducerImp;
+import com.microsoft.aad.msal4j.ClientCredentialFactory;
+import com.microsoft.aad.msal4j.ClientCredentialParameters;
+import com.microsoft.aad.msal4j.ConfidentialClientApplication;
+import com.microsoft.aad.msal4j.IAuthenticationResult;
+import com.microsoft.graph.concurrency.ChunkedUploadProvider;
+import com.microsoft.graph.concurrency.IProgressCallback;
+import com.microsoft.graph.core.ClientException;
+import com.microsoft.graph.models.extensions.Attachment;
+import com.microsoft.graph.models.extensions.AttachmentItem;
+import com.microsoft.graph.models.extensions.EmailAddress;
+import com.microsoft.graph.models.extensions.FileAttachment;
+import com.microsoft.graph.models.extensions.IGraphServiceClient;
+import com.microsoft.graph.models.extensions.ItemBody;
+import com.microsoft.graph.models.extensions.Message;
+import com.microsoft.graph.models.extensions.Recipient;
+import com.microsoft.graph.models.extensions.UploadSession;
+import com.microsoft.graph.models.generated.AttachmentType;
+import com.microsoft.graph.models.generated.BodyType;
+import com.microsoft.graph.requests.extensions.AttachmentCollectionPage;
+import com.microsoft.graph.requests.extensions.AttachmentCollectionRequest;
+import com.microsoft.graph.requests.extensions.GraphServiceClient;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.BooleanUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.validation.constraints.NotBlank;
+import java.io.InputStream;
+import java.util.Base64;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Implementation of an email producer that is geared towards Microsoft
+ * Office 365, using their Graph API and OAuth2.
+ *
+ * @config office-365-mail-producer
+ */
+@XStreamAlias("office-365-mail-producer")
+@AdapterComponent
+@ComponentProfile(summary = "Send email using a Microsoft Office 365 account using the Microsoft Graph API", tag = "producer,email,o365,microsoft,office,outlook,365")
+@DisplayOrder(order = { "applicationId", "tenantId", "clientSecret", "username", "subject", "toRecipients", "ccRecipients", "bccRecipients", "save" })
+public class O365MailProducer extends ProduceOnlyProducerImp
+{
+  private static final String SCOPE = "https://graph.microsoft.com/.default";
+
+  @Getter
+  @Setter
+  @NotBlank
+  private String applicationId;
+
+  @Getter
+  @Setter
+  @NotBlank
+  private String tenantId;
+
+  @Getter
+  @Setter
+  @NotBlank
+  private String clientSecret;
+
+  @Getter
+  @Setter
+  @NotBlank
+  @InputFieldHint(expression = true)
+  private String username;
+
+  @Getter
+  @Setter
+  @InputFieldHint(expression = true)
+  private String subject;
+
+  @Getter
+  @Setter
+  @NotBlank
+  @InputFieldHint(friendly = "Comma separated list of email addresses", expression = true)
+  private String toRecipients;
+
+  @Getter
+  @Setter
+  @AdvancedConfig
+  @InputFieldHint(friendly = "Comma separated list of email addresses", expression = true)
+  private String ccRecipients;
+
+  @Getter
+  @Setter
+  @AdvancedConfig
+  @InputFieldHint(friendly = "Comma separated list of email addresses", expression = true)
+  private String bccRecipients;
+
+  @Getter
+  @Setter
+  @AdvancedConfig
+  @InputFieldHint(friendly = "Save message to sent items?")
+  @InputFieldDefault("true")
+  private Boolean save;
+
+  private transient ConfidentialClientApplication confidentialClientApplication;
+
+  @Override
+  public void prepare() throws CoreException
+  {
+    try
+    {
+      confidentialClientApplication = ConfidentialClientApplication.builder(applicationId,
+              ClientCredentialFactory.createFromSecret(clientSecret))
+              .authority(tenant())
+              .build();
+    }
+    catch (Exception e)
+    {
+      log.error("Could not identify Azure application or tenant", e);
+      throw new CoreException(e);
+    }
+  }
+
+  @Override
+  protected void doProduce(AdaptrisMessage adaptrisMessage, String endpoint) throws ProduceException
+  {
+    String user = adaptrisMessage.resolve(username);
+
+    log.debug("Sending mail via Office365 as user " + user);
+
+    try
+    {
+      IAuthenticationResult iAuthResult = confidentialClientApplication.acquireToken(ClientCredentialParameters.builder(Collections.singleton(SCOPE)).build()).join();
+      log.trace("Access token: " + iAuthResult.accessToken());
+      IGraphServiceClient graphClient = GraphServiceClient.builder().authenticationProvider(request -> request.addHeader("Authorization", "Bearer " + iAuthResult.accessToken())).buildClient();
+
+      Message outlookMessage = new Message();
+      outlookMessage.subject = adaptrisMessage.resolve(subject);
+
+      ItemBody body = new ItemBody();
+      body.contentType = BodyType.TEXT;
+      body.content = adaptrisMessage.getContent();
+      outlookMessage.body = body;
+
+      outlookMessage.toRecipients = resolveRecipients(adaptrisMessage, toRecipients);
+      outlookMessage.ccRecipients = resolveRecipients(adaptrisMessage, ccRecipients);
+      outlookMessage.bccRecipients = resolveRecipients(adaptrisMessage, bccRecipients);
+
+      // TODO Add any custom headers.
+
+      if (adaptrisMessage instanceof MultiPayloadAdaptrisMessage)
+      {
+        outlookMessage.isDraft = true;
+        Message draftMessage = graphClient.users(user).messages().buildRequest().post(outlookMessage);
+
+        MultiPayloadAdaptrisMessage multiPayloadAdaptrisMessage = (MultiPayloadAdaptrisMessage)adaptrisMessage;
+        String id = multiPayloadAdaptrisMessage.getCurrentPayloadId();
+        for (String name : multiPayloadAdaptrisMessage.getPayloadIDs())
+        {
+          if (name.equals(id))
+          {
+            continue;
+          }
+          long size = multiPayloadAdaptrisMessage.getSize(name);
+          /*
+           * Depending on the size of the file, you can choose one of
+           * two ways to attach a file to a message:
+           *  - If the file size is under 4 MB, you can do a single POST
+           *    on the attachments navigation property of the message.
+           *    The successful POST response includes the ID of the file
+           *    attached to the message.
+           *  - If the file size is between 3MB and 150MB, create an
+           *    upload session, and iteratively use PUT to upload ranges
+           *    of bytes of the file until you have uploaded the entire
+           *    file. A header in the final successful PUT response
+           *    includes a URL with the attachment ID.
+           */
+          if (size < 4 * FileUtils.ONE_MB)
+          {
+            FileAttachment attachment = new FileAttachment();
+            attachment.oDataType = "#microsoft.graph.fileAttachment";
+            attachment.name = name;
+            attachment.size = (int)size;
+            attachment.contentType = "application/octet-stream";
+            attachment.contentBytes = Base64.getEncoder().encode(multiPayloadAdaptrisMessage.getPayload(name));
+            graphClient.users(user).messages(draftMessage.id).attachments().buildRequest().post(attachment);
+
+          }
+/*          else
+          {
+            AttachmentItem attachment = new AttachmentItem();
+            attachment.attachmentType = AttachmentType.FILE;
+            attachment.name = name;
+            attachment.size = size;
+            attachment.contentType = "application/octet-stream";
+
+            UploadSession uploadSession = graphClient.users(user).messages(draftMessage.id).attachments().createUploadSession(attachment).buildRequest().post();
+            ChunkedUploadProvider<AttachmentItem> chunkedUploadProvider = new ChunkedUploadProvider<>(uploadSession, graphClient, multiPayloadAdaptrisMessage.getInputStream(name), attachment.size, AttachmentItem.class);
+            chunkedUploadProvider.upload(new IProgressCallback()
+            {
+              @Override
+              public void success(Object o)
+              {
+                graphClient.users(user).messages(draftMessage.id).send().buildRequest().post();
+              }
+
+              @Override
+              public void failure(ClientException ex)
+              {
+                log.error("Could not process attachment {} for message {} : {}", attachment.name, adaptrisMessage.getUniqueId(), ex);
+              }
+
+              @Override
+              public void progress(long current, long max)
+              {
+                // do nothing
+              }
+            });
+          }*/
+        }
+        graphClient.users(user).messages(draftMessage.id).send().buildRequest().post();
+      }
+      else
+      {
+        graphClient.users(user).sendMail(outlookMessage, save()).buildRequest().post();
+      }
+    }
+    catch (Exception e)
+    {
+      log.error("Exception processing Outlook message", e);
+      throw new ProduceException(e);
+    }
+  }
+
+  @Override
+  public String endpoint(AdaptrisMessage adaptrisMessage)
+  {
+    return adaptrisMessage.resolve(toRecipients);
+  }
+
+  private static List<Recipient> resolveRecipients(AdaptrisMessage adaptrisMessage, String recipients)
+  {
+    if (StringUtils.isEmpty(recipients))
+    {
+      return null;
+    }
+    List<Recipient> recipientList = new LinkedList<>();
+    for (String address : adaptrisMessage.resolve(recipients).split(","))
+    {
+      Recipient recipient = new Recipient();
+      EmailAddress emailAddress = new EmailAddress();
+      emailAddress.address = address;
+      recipient.emailAddress = emailAddress;
+      recipientList.add(recipient);
+    }
+    return recipientList;
+  }
+
+  private String tenant()
+  {
+    return String.format("https://login.microsoftonline.com/%s", tenantId);
+  }
+
+  private boolean save()
+  {
+    return BooleanUtils.toBooleanDefaultIfNull(save, true);
+  }
+}
