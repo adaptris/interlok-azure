66a07fedc3ae17ccc190d3dc2fc4e6aade01e172
Ashley Morgan Anderson <amanderson@albinoloverats.net>
2020-11-11 10:01:51 +0000

    INTERLOK-3463 Add service to wrap producer

---
+++ interlok-azure-datalake/src/main/java/com/adaptris/interlok/azure/datalake/DataLakeUploadService.java
@@ -0,0 +1,116 @@
+package com.adaptris.interlok.azure.datalake;
+
+import com.adaptris.annotation.AdapterComponent;
+import com.adaptris.annotation.ComponentProfile;
+import com.adaptris.annotation.DisplayOrder;
+import com.adaptris.annotation.InputFieldHint;
+import com.adaptris.core.AdaptrisMessage;
+import com.adaptris.core.CoreException;
+import com.adaptris.core.ServiceException;
+import com.adaptris.core.ServiceImp;
+import com.adaptris.interlok.azure.DataLakeConnection;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+/**
+ * Upload files to Microsoft Data Lake.
+ *
+ * @config azure-data-lake-upload-service
+ */
+@XStreamAlias("azure-data-lake-upload-service")
+@AdapterComponent
+@ComponentProfile(summary = "Put data into a Azure Data Lake", tag = "service,azure,data lake,data,lake")
+@DisplayOrder(order = { "fileSystem", "path", "filename" })
+public class DataLakeUploadService extends ServiceImp
+{
+  /**
+   * A connection to an Azure Data Lake.
+   */
+  @Getter
+  @Setter
+  @NotNull
+  private DataLakeConnection connection;
+  
+  /**
+   * The Data Lake file system to access.
+   */
+  @Getter
+  @Setter
+  @NotBlank
+  @InputFieldHint(expression = true)
+  private String fileSystem;
+
+  /**
+   * The path to poll for files.
+   */
+  @Getter
+  @Setter
+  @NotBlank
+  @InputFieldHint(expression = true)
+  private String path;
+
+  /**
+   * The name of the file to put into the Data Lake.
+   */
+  @Getter
+  @Setter
+  @NotBlank
+  @InputFieldHint(expression = true)
+  private String filename;
+  
+  /**
+   * {@inheritDoc}.
+   */
+  @Override
+  protected void initService()
+  {
+    /* do nothing */
+  }
+
+  /**
+   * {@inheritDoc}.
+   */
+  @Override
+  protected void closeService()
+  {
+    /* do nothing */
+  }
+
+  /**
+   * Upload the given message to the Data Lake.
+   *
+   * @param adaptrisMessage The message to upload.
+   * @throws ServiceException If there was an issue uploading the file.
+   */
+  @Override
+  public void doService(AdaptrisMessage adaptrisMessage) throws ServiceException
+  {
+    try
+    {
+      DataLakeProducer producer = new DataLakeProducer();
+      producer.registerConnection(connection);
+      producer.setFileSystem(adaptrisMessage.resolve(fileSystem));
+      producer.setPath(adaptrisMessage.resolve(path));
+      producer.setFilename(adaptrisMessage.resolve(filename));
+      producer.doProduce(adaptrisMessage, null);
+    }
+    catch (Exception e)
+    {
+      log.error("Could not upload file", e);
+      throw new ServiceException(e);
+    }
+  }
+
+  /**
+   * {@inheritDoc}.
+   */
+  @Override
+  public void prepare()
+  {
+    /* do nothing */
+  }
+}
